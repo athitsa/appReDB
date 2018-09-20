b0e62a8690e70f5a0aba31bb1425a053380b0af1
athitA <athitsa58@email.nu.ac.th>
2018-09-20 19:09:23 +0700

    update ui app

--- App.js
+++ App.js
@@ -1,34 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+/* Other dependency libs */
+import axios from 'axios';
+//import HTMLView from 'react-native-htmlview';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  /* Before app load */
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      result: ''
+    };
+
+  }
+
+  /* Event  */
+  onClickButton() {
+    /* Exsample setstate */
+    //const url = `http://178.128.56.60:3030/api/${this.state.query}`
+    this.setState({query: ''})
+    const url = `https://www.google.co.th/search?q${this.state.query}`
+    this.setState({query: url})
+    /* axios start sent get request */
+    axios.get(url)
+      .then(res => this.setState({result: res.data}))
+  }
+
+  /* Render UI */
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Start App!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {/* Text1 */}
+        <Text 
+          style={styles.startText}>Search from Google 
+        </Text>
+
+        {/* Textinput */}
+        <TextInput
+          placeholder="Key search ... " 
+          onChangeText={(query) => this.setState({query})} >
+        </TextInput>
+
+        {/* Button */}
+        <Button 
+          onPress={this.onClickButton.bind(this)} 
+          title="Submit" />
+
+        {/* Text2 */}
+        <Text 
+          style={styles.startText}>
+          {this.state.query}
+          {/* {this.state.result} */}
+        </Text>
+
+        {/* Show Website from HTML string */}
+        {/* <HTMLView
+          value={this.state.result}
+        /> */}
       </View>
     );
   }
-}
+} // END App
 
+// CSS VARIABLE
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -36,14 +74,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  startText: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
